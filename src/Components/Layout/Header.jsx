1b6f4fc2788e52073fbcc5eccdece71167214a1e
Viktor Pavlovic <viktorpavlovic117@gmail.com>
2023-04-01 22:58:07 +0200

    starting project order meal app

---
+++ src/Components/Layout/Header.jsx
@@ -0,0 +1,21 @@
+import React, { Fragment } from "react";
+import mealsImg from "../../assets/meals.jpg";
+import HeaderCartButton from "./HeaderCartButton";
+
+import classes from "./header.module.css";
+
+const Header = (props) => {
+  return (
+    <Fragment>
+      <header className={classes.header}>
+        <h1>ReactMeals</h1>
+        <HeaderCartButton />
+      </header>
+      <div className={classes["main-image"]}>
+        <img src={mealsImg} alt="food" />
+      </div>
+    </Fragment>
+  );
+};
+
+export default Header;
