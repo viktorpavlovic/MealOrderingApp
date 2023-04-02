83d0f500b06d3bafe21fb37ebcddcefd271bcc72
Viktor Pavlovic <viktorpavlovic117@gmail.com>
2023-04-02 21:28:40 +0200

    updating meal ordering app

--- src/App.js
+++ src/App.js
@@ -1,15 +1,26 @@
-import { Fragment } from "react";
+import { useState } from "react";
 import Header from "./Components/Layout/Header";
 import Meals from "./Components/Meals/Meals";
+import Cart from "./Components/Cart/Cart";
+import CartProvider from "./store/CartProvider";
 
 function App() {
+  const [cartIsShown, setCartIsShown] = useState(false);
+
+  const showCartHandler = () => {
+    setCartIsShown(true);
+  };
+  const hideCartHandler = () => {
+    setCartIsShown(false);
+  };
   return (
-    <Fragment>
-      <Header />
+    <CartProvider>
+      {cartIsShown && <Cart onClose={hideCartHandler} />}
+      <Header onShowCart={showCartHandler} />
       <main>
         <Meals />
       </main>
-    </Fragment>
+    </CartProvider>
   );
 }
 
--- src/Components/Meals/MealItem/MealItem.jsx
+++ src/Components/Meals/MealItem/MealItem.jsx
@@ -1,7 +1,20 @@
+import { useContext } from "react";
+import MealItemForm from "./MealItemForm";
 import classes from "./meal-item.module.css";
+import CartContext from "../../../store/CartContext";
 
 const MealItem = (props) => {
+  const cartCtx = useContext(CartContext);
   const price = `$${props.price.toFixed(2)}`;
+
+  const addToCartHandler = (amount) => {
+    cartCtx.addItem({
+      id: props.id,
+      name: props.name,
+      amount: amount,
+      price: props.price,
+    });
+  };
 
   return (
     <li className={classes.meal}>
@@ -10,7 +23,9 @@ const MealItem = (props) => {
         <div className={classes.description}>{props.description}</div>
         <div className={classes.price}>{price}</div>
       </div>
-      <div></div>
+      <div>
+        <MealItemForm onAddToCart={addToCartHandler} />
+      </div>
     </li>
   );
 };
